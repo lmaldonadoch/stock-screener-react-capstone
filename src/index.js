610ba38fd26b5e019d3d71cfe499c68961a00b2b
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-23 14:12:08 -0500

    Managed to make chartjs display a chart

--- src/index.js
+++ src/index.js
@@ -34,14 +34,11 @@ const initialState = {
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
 
 function Details() {
+  const data = {
+    labels: ['11:00', '12:00'],
+    datasets: [
+      {
+        label: 'AAPL',
+        data: [200, 210],
+        backgroundColor: 'rgba(232,89,0, 0.4)',
+        borderColor: 'rgba(232,89,0, 1)',
+        pointBorderColor: 'rgba(232,89,0, 0.5)',
+      },
+    ],
+  };
   return (
     <div className="Details">
-      <header className="Details-header">
-        <p>Details</p>
-      </header>
+      <Line data={data} />
     </div>
   );
 }
