d632d9a8ad7e2d6889d9f4cdcc9dffd6826f39bc
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-28 12:56:15 -0500

    Started adding tests for components

--- src/tests/Filter.test.js
+++ src/tests/Filter.test.js
@@ -2,14 +2,36 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Filter from '../components/Filter';
 import Wrapper from './Wrapper';
+import { mount, configure } from 'enzyme';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import Adapter from 'enzyme-adapter-react-16';
 
 test('Filter is rendered', () => {
   const component = renderer.create(
     <Wrapper>
       <Filter />
-    </Wrapper>,
+    </Wrapper>
   );
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+const mockStore = configureMockStore([thunk]);
+configure({ adapter: new Adapter() });
+
+describe('NAV', () => {
+  it('should find filter', () => {
+    const store = mockStore({
+      filter: null,
+    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <Filter />
+      </Provider>
+    );
+    expect(wrapper.find('filter')).toEqual({});
+  });
 });
--- src/tests/StocksForm.test.js
+++ src/tests/StocksForm.test.js
@@ -2,14 +2,23 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Form from '../containers/StocksForm';
 import Wrapper from './Wrapper';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
 test('Form i rendered', () => {
   const component = renderer.create(
     <Wrapper>
       <Form />
-    </Wrapper>,
+    </Wrapper>
   );
 
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+describe('Form', () => {
+  it('is receiving and runing JS', () => {
+    expect(2 + 3).toEqual(5);
+  });
 });
